56af26a7ccac13798bbe5fd9acb49eb534d7875f
Damynas <povdam6@gmail.com>
2023-02-18 16:30:53 +0200

    React Router Dom and Styled Components libraries added and examples implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavigationBar = styled.div`
+  height: 30px;
+  color: black;
+  font-weight: bold;
+  background-color: purple;
+`;
+
 const App = () => {
-  return <div>ACT Webapp</div>;
+  return (
+    <>
+      <NavigationBar>Navigation Bar</NavigationBar>
+      <Router>
+        <Routes>
+          <Route path='/'>
+            <Route
+              index
+              element={<div>ACT Webapp</div>}
+            />
+            <Route
+              path='login'
+              element={<div>Login</div>}
+            />
+            <Route
+              path='register'
+              element={<div>Register</div>}
+            />
+          </Route>
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;

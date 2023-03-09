e3877727272d80ed2b785b7c91c87897d524ea21
Damynas <povdam6@gmail.com>
2023-03-10 00:33:13 +0200

    Fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { NavigationBar, Theme } from './components';
 import { Registration, UserInteface } from './pages';
-import Home from './pages/Home/Home';
 import { Login } from './pages/Login';
 
 const App = () => {

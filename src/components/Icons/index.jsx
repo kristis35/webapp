eb428dac0afe2fddccca3808839fee34b8837d31
Damynas <povdam6@gmail.com>
2023-03-16 21:31:09 +0200

    New icons added and base icon component created

--- src/components/Icons/index.jsx
+++ src/components/Icons/index.jsx
@@ -1,1 +1,3 @@
-export { default as MenuIcon } from './MenuIcon';
+export { default as MenuIcon } from './menu-icon';
+export { default as ArrowLeftIcon } from './arrow-left-icon';
+export { default as ArrowRightIcon } from './arrow-right-icon';

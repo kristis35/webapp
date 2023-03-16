901c44f5b6b2d360a171730bf4a57b2ba0078544
Damynas <povdam6@gmail.com>
2023-03-16 21:32:13 +0200

    Context added

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { NavigationBar, Theme } from './components';
 import { Registration, UserInteface } from './pages';
-import { Login } from './pages/Login';
+import { Login } from './pages';
+import { DataContext } from './utils';
 
 const App = () => {
+  const apiName = 'http://localhost:8080';
   return (
-    <Theme>
-      <Router>
-        <NavigationBar />
-        <Routes>
-          <Route path='/'>
-            <Route
-              index
-              element={<div>ACT Webapp</div>}
-            />
-            <Route
-              path='login'
-              element={<Login />}
-            />
-            <Route
-              path='register'
-              element={<Registration />}
-            />
-            <Route
-              path='ui'
-              element={<UserInteface />}
-            />
-          </Route>
-        </Routes>
-      </Router>
-    </Theme>
+    <DataContext.Provider value={{ API: apiName }}>
+      <Theme>
+        <Router>
+          <NavigationBar />
+          <Routes>
+            <Route path='/'>
+              <Route
+                index
+                element={<div>ACT Webapp</div>}
+              />
+              <Route
+                path='login'
+                element={<Login />}
+              />
+              <Route
+                path='register'
+                element={<Registration />}
+              />
+              <Route
+                path='ui'
+                element={<UserInteface />}
+              />
+            </Route>
+          </Routes>
+        </Router>
+      </Theme>
+    </DataContext.Provider>
   );
 };
 
